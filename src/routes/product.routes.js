85f9581cf83b48b652c0705dd52a7d4635b47ca7
micaelabenitez <mabenittez@gmail.com>
2024-09-12 00:31:32 -0300

    add endpoints get by id, put & delete of instruments

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 const getInstrument = async (req, res, next) => {
     let instrument;
     const { id } = req.params;
-
+   
     try {
         instrument = await Instrument.findById(id)
         if(!instrument) {
@@ -59,5 +59,39 @@ router.post('/', async (req, res) => {
         res.status(400).json({message: error.message})
     }
 })  
+
+router.get('/:id', getInstrument, async(req, res) => {
+    res.json(res.book)
+}) 
+
+router.put('/:id', getInstrument, async(req, res) => {
+    try {
+        const instrument = res.instrument
+        instrument.name  = req.body.name || instrument.name;
+        instrument.marca  = req.body.marca || instrument.marca;
+        instrument.tipo  = req.body.tipo || instrument.tipo;
+        instrument.subtipo  = req.body.subtipo || instrument.subtipo;
+        instrument.descripcion  = req.body.descripcion || instrument.descripcion;
+        instrument.precio  = req.body.precio || instrument.precio;                
+        instrument.img_url  = req.body.img_url || instrument.img_url;  
+        
+        const updatedInstrument = await instrument.save()
+        res.json(updatedInstrument)
+    } catch (error) {
+        res.status(400).json({message: error.message})    
+    }
+})
+
+router.delete('/:id', getInstrument, async(req, res) => {
+    try {
+        const instrument = res.instrument
+        await instrument.deleteOne({_id: book._id})
+        res.json({message: 'El instrumento ha sido eliminado correctamente'})
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
 
 export default router;
